62ddae767d51507f5cc49d4e623ab0dfd72bc0be
Stanislav Lapshin <sm.lapshin@gmail.com>
2022-11-21 16:19:16 +0300

    refactor(ci): improvements

--- docker/server/celery_worker.sh
+++ docker/server/celery_worker.sh
@@ -2,15 +2,14 @@
 
 set -o errexit
 
-_CELERY_OPTS="-A server.celery_app worker"
-if [ "${CELERY_CONCURRENCY:-}" != "" ]
-then
-  _CELERY_OPTS="${_CELERY_OPTS} --concurrency ${CELERY_CONCURRENCY}"
+_CELERY_OPTS=()
+if [ "${CELERY_CONCURRENCY:-}" != "" ]; then
+  _CELERY_OPTS+=(--concurrency "${CELERY_CONCURRENCY}")
 fi
 
-if [ "${CELERY_QUEUES:-}" != "" ]
-then
-  _CELERY_OPTS="${_CELERY_OPTS} -Q ${CELERY_QUEUES}"
+if [ "${CELERY_QUEUES:-}" != "" ]; then
+  _CELERY_OPTS+=(-Q "${CELERY_QUEUES}")
 fi
 
-celery ${_CELERY_OPTS}
+celery -A server.celery_app worker \
+  "${_CELERY_OPTS[@]}"
